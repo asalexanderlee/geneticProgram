a087ed593ad06ae643d81ab0d89b77e783fc54b4
alherronWC <alexanderfherron@gmail.com>
2017-02-16 21:53:12 -0500

    Add files via upload

--- main.java
+++ main.java
@@ -4,51 +4,87 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class main {
-	
-	private static Tree[] population;
-	
-	private static String[] getIntArray(int numInt){
-		String[] intArray = new String[numInt];
-		for (int i = 1; i < numInt+1; i++){
-			intArray[i-1] = i + "";
-		}
-		return intArray;
-	}
-	public static void main(String args[]){
-		
-		String[] possOperators = {"+", "-", "*", "/"};
-		String[] ints = getIntArray(50);
-		int maxDepth = 2;
-		String method = "grow";
-		Tree test = new Tree(possOperators, ints, maxDepth, method);
-		test.str();
-		test.mutate(ints, possOperators);
-		
-		population = initializePop(possOperators, ints, maxDepth);
-		
-		//initialize population with 4 random trees //Ash
-		
-		//check fitness to see if they work
-		//best variable to compare fitness  //Al
-		//tournament to choose 2 most fit trees and crossover //Al
-		//function that returns crossover 90% of the time, mutation 10% //L
-		//bloat,etc //L
-		//run until find answer
-	}
-	
-	public static Tree[] initializePop(String[] ops, String[] ints, int depth){
-		Tree[] pop = new Tree[4];
-		
-		for (int i = 0; i < 4; i++){
-			if (i < 2){
-				pop[i] = new Tree(ops, ints, depth, "full");
-			}else{
-				pop[i] = new Tree(ops, ints, depth, "grow");
-			}
-		}
-		
-		return pop;
-	}
+ 
+ private static Tree[] population;
+ 
+ private static String[] getIntArray(int numInt){
+  String[] intArray = new String[2*(numInt) + 1];
+  for (int i = -numInt; i < numInt+1; i++){
+   intArray[i+numInt] = i + "";
+  }
+  return intArray;
+ }
+ 
+ 
+ public static Tree createSymbReg(List<Double> xValues, List<Double> yValues,String[] termSet, String[] funcSet){
+  
+  for (int i = 0; i < 25; i++){
+    System.out.println(i);
+   call(xValues, yValues, termSet, funcSet);
+  }
+  int best = -1;
+  double min = Integer.MAX_VALUE;
+  for (int i = 0; i < population.length; i++){
+   double fitness = getFitness(xValues,yValues,population[i]);
+   if (fitness < min){
+    min = fitness;
+    best = i;
+   }
+  }
+  System.out.println(min);
+  return population[best];
+ }
+ 
+ public static Tree[] initializePop(String[] ops, String[] ints, int depth){
+  Tree[] pop = new Tree[4];
+  
+  for (int i = 0; i < 4; i++){
+   if (i < 2){
+    pop[i] = new Tree(ops, ints, depth, "full");
+   }else{
+    pop[i] = new Tree(ops, ints, depth, "grow");
+   }
+  }
+  
+  return pop;
+ }
+ 
+ public static void call(List<Double> xValues, List<Double> yValues,String[] termSet, String[] funcSet){
+   
+  double[] probDis = getProbDistribution(xValues,yValues, population);
+  Tree[] newPop = new Tree[4];
+  for (int i = 0; i < population.length; i++){
+    
+   Random rand = new Random();  
+   double prob = rand.nextDouble(); 
+   if (prob < .10){
+    Tree child = (chooseRandTree(probDis,population)).treeCopy();
+    
+       child.mutate(termSet,funcSet);
+       while (Double.isNaN(child.getY(5))){
+            child.mutate(termSet,funcSet);
+    }
+       newPop[i] = child;
+       //System.out.println("Mutation");
+    //Tree child = Mutation();
+   }
+   else{
+     
+    Tree child = crossover(probDis,population);
+    while (Double.isNaN(child.getY(5))){
+         child = crossover(probDis,population);
+    }
+    newPop[i] = child;
+    //System.out.println("Crossover");
+    //Tree child = new Tree; 
+    //child = Crossover(); 
+   }
+   
+   //Tree[].add(child) 
+  }
+  population = newPop;
+  
+ }
   
   public static String FILENAME = "dataset1.csv";
   
@@ -95,12 +131,23 @@ public class main {
    */
   
   public static double getFitness(List<Double> xValues, List<Double> yValues, Tree tree){
+   //tree.str();
+   //System.out.println();
+   //ystem.out.println();
     double fitness = 0;
     for (int i = 0; i < xValues.size(); i++){
-      double error = Math.abs(tree.getY(xValues.get(i)) - yValues.get(i));
-      fitness += error * error;
+      if (Double.isNaN(tree.getY(xValues.get(i)))) continue;
+      double error = tree.getY(xValues.get(i)) - yValues.get(i);
+     
+      // System.out.println(error);
+      fitness += (error * error) ;
+     //System.out.println(fitness);
+      
+      
     }
-    return fitness;
+    //System.out.println(fitness);
+    
+    return fitness / xValues.size();
     
   }
   
@@ -120,8 +167,9 @@ public class main {
     double fitnessSum = 0;
     double[] fitnessLst = new double[trees.length];
     for (int i = 0; i < trees.length; i++){
-      double fitness = getFitness(xValues, yValues, trees[i]);
+      double fitness = 1.0 / (getFitness(xValues, yValues, trees[i])) ;
       fitnessLst[i] = fitness;
+      //System.out.println(fitness);
       fitnessSum += fitness;
     }
     double[] distribution = new double[trees.length];
@@ -150,7 +198,9 @@ public class main {
    */
   public static Tree chooseRandTree(double[] distribution, Tree[] trees){
     double r = new Random().nextDouble();
-    for (int i = 0; i < distribution.length - 1; i++){
+    //System.out.println(r);
+    //System.out.println(Arrays.toString(distribution));
+    for (int i = 0; i < distribution.length; i++){
       if (r <= distribution[i]){
         return trees[i];
       }
@@ -173,26 +223,18 @@ public class main {
    */
   public static Tree reproduce(Tree tree1, Tree tree2){
     Tree tree1Copy = tree1.treeCopy();
+    if (tree2 == null) System.out.println("know");
+    if (tree1 == null) System.out.println("know");
     Tree tree2Copy = tree2.treeCopy();
     //select tree1 one crossover point
     //select terminal node as crossover with 10 percent probability. o\w pick function
-    double r1 = new Random().nextDouble();
+    
     Node tree1Cross;
-    if (r1 < 0.1){
-      tree1Cross = tree1Copy.getCross("terminal");
-    }
-    else {
-      tree1Cross = tree1Copy.getCross("function");
-    }
+    tree1Cross = getCrossType(tree1Copy);
     //select tree2 crossover points
-    double r2 = new Random().nextDouble();
+   
     Node tree2Cross;
-    if (r2 < 0.1){
-      tree2Cross = tree2Copy.getCross("terminal");
-    }
-    else {
-       tree2Cross = tree2Copy.getCross("function");
-    }
+    tree2Cross = getCrossType(tree2Copy);
    
     tree1Cross.setData(tree2Cross.getData());
     tree1Cross.setLeft(tree2Cross.getLeft());
@@ -201,6 +243,19 @@ public class main {
     
     
   }
+  
+  public static Node getCrossType(Tree tree){
+     double r = new Random().nextDouble();
+    if (r < 0.1){
+      return tree.getCross("terminal");
+    }
+    else {
+      return tree.getCross("function");
+    }
+    
+  }
+  
+  
   
    /* Chooses two trees to reproduce and then shares their genes
    *
@@ -214,21 +269,45 @@ public class main {
   public static Tree crossover(double[] distribution, Tree[] trees){
     Tree tree1 = chooseRandTree(distribution, trees);
     Tree tree2 = chooseRandTree(distribution, trees);
+    if (tree2 == null) System.out.println("Null1");
     while (tree1.equals(tree2)){
       tree2 = chooseRandTree(distribution,trees);
+      if (tree2 == null) System.out.println("Null2");
     }
     return reproduce(tree1, tree2);
     
   }
   
+  public static void main(String args[]){
+   
+     
+  List<Double> xValues = new ArrayList<Double>();
+    List<Double> yValues = new ArrayList<Double>();
+    readCSV(xValues, yValues);
+  String[] possOperators = {"+", "-", "*", "/"};
+  String[] vars = {"x"};
+  String[] terms = getIntArray(50);
+  int maxDepth = 3;
+  population = initializePop(possOperators, terms, maxDepth);
+  
+  String method = "full";
+  createSymbReg(xValues, yValues, terms, possOperators).str();
   
   
- private static String[] getIntArray(int numInt){
-  String[] intArray = new String[numInt];
-  for (int i = 1; i < numInt+1; i++){
-   intArray[i-1] = i + "";
+  /*
+  for (int i = 0; i < 4; i++){
+   population[i].printPostOrder();
+   System.out.println();
   }
-  return intArray;
+  System.out.println();
+  readCSV(xValues,yValues);
+  call(xValues,yValues,possOperators,ints);
+  for (int i = 0; i < 4; i++){
+   population[i].printPostOrder();
+   System.out.println();
+  }*/
+  
+  
  }
-	
+ 
 }
